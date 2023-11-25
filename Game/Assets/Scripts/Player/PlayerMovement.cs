01eca8fdef3f0ac9b2a57d0d91cf17cab1c47a63
PedroHenriqueDiasBatista <phdiasbatistaprofessional@gmail.com>
2023-11-25 09:41:36 -0300

    feat: Create Player Animations

--- Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private enum MovementState { IDLE, WALK, JUMP }
+
     private Rigidbody2D _rb;
     private BoxCollider2D _coll;
     private SpriteRenderer _spr;
+    private Animator _anim;
+
+    private float dirX = 0f;
 
     [SerializeField] private LayerMask groundLayer;
 
@@ -16,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _coll = GetComponent<BoxCollider2D>();
+        _anim = GetComponent<Animator>();
 
         _spr = GetComponentInChildren<SpriteRenderer>();
     }
@@ -23,13 +29,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         _rb.velocity = new Vector2(dirX * moveSpeed, _rb.velocity.y);
-
         if (Input.GetButtonDown("Jump") && IsGrounded()) _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
 
-        if (dirX > 0) _spr.flipX = false;
-        else if (dirX < 0) _spr.flipX = true;
+        UpdateAnimationStateMachine();
     }
 
     /// <summary>
@@ -39,5 +43,31 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(_coll.bounds.center, _coll.bounds.size, 0f, Vector2.down, .1f, groundLayer);
+    }
+
+    /// <summary>
+    /// Cria máquina de estados para gerenciamento das animações de movimentacao do personagem
+    /// </summary>
+    private void UpdateAnimationStateMachine()
+    {
+        MovementState currentState = MovementState.IDLE;
+        
+        //Verifica se o personagem está se movendo
+        if (dirX != 0)
+        {
+            if (dirX > 0) _spr.flipX = false;
+            else if (dirX < 0) _spr.flipX = true;
+
+            currentState = MovementState.WALK;
+        }
+        else
+        {
+            currentState = MovementState.IDLE;
+        }
+
+        //Verifica se o personagem está no ar
+        if(!IsGrounded()) currentState = MovementState.JUMP;
+
+        _anim.SetInteger("state", (int)currentState);
     }
 }
